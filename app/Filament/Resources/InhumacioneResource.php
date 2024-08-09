65652dfb196802906ccb15d85fe90c031960ae90
alanezfranz <alanezfranz@gmail.com>
2024-08-09 18:25:40 -0400

    formularios por pasos inhumaciones

--- app/Filament/Resources/InhumacioneResource.php
+++ app/Filament/Resources/InhumacioneResource.php
@@ -12,6 +12,13 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Filament\Forms\Components\Wizard;
+
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\DatePicker;
+use Filament\Forms\Components\Textarea;
+
 
 class InhumacioneResource extends Resource
 {
@@ -21,152 +28,125 @@ class InhumacioneResource extends Resource
 
     public static function form(Form $form): Form
     {
-        return $form
-        ->schema([
-            Forms\Components\Section::make('Información del Difunto')
-                ->schema([
-                    Forms\Components\TextInput::make('nombre_difunto')
-                        ->label('Nombre Difunto')
-                        ->required(),
-        
-                    Forms\Components\Select::make('sexo')
-                        ->label('Sexo')
-                        ->options([
-                            'masculino' => 'Masculino',
-                            'femenino' => 'Femenino',
-                        ])
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('edad')
-                        ->label('Edad')
-                        ->required()
-                        ->numeric(),
-        
-                    Forms\Components\Select::make('estado_civil')
-                        ->label('Estado Civil')
-                        ->options([
-                            'soltero' => 'Soltero(a)',
-                            'casado' => 'Casado(a)',
-                            'divorciado' => 'Divorciado(a)',
-                            'viudo' => 'Viudo(a)',
-                        ])
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('nacionalidad')
-                        ->label('Nacionalidad')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('diagnostico_fallecimiento')
-                        ->label('Diagnóstico Fallecimiento')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('medico')
-                        ->label('Médico')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('orc')
-                        ->label('ORC')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('libro')
-                        ->label('Libro')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('folio')
-                        ->label('Folio')
-                        ->required(),
-        
-                    Forms\Components\DatePicker::make('fecha_inhumacion')
-                        ->label('Fecha Inhumación')
-                        ->required(),
-        
-                    Forms\Components\DatePicker::make('fecha_vencimiento')
-                        ->label('Fecha Vencimiento')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('dia')
-                        ->label('Día')
-                        ->required(),
-        
-                    Forms\Components\Textarea::make('descripcion_nicho')
-                        ->label('Descripción de Nicho')
-                        ->required(),
-                ]),
-        
-            Forms\Components\Section::make('Ubicación')
-                ->schema([
-                    Forms\Components\TextInput::make('fila_ubicacion')
-                        ->label('Fila Ubicación')
-                        ->required()
-                        ->maxLength(255),
-        
-                    Forms\Components\TextInput::make('sector_ubicacion')
-                        ->label('Sector Ubicación')
-                        ->required()
-                        ->maxLength(255),
-        
-                    Forms\Components\TextInput::make('nro_ubicacion')
-                        ->label('Número Ubicación')
-                        ->required()
-                        ->maxLength(255),
-                ]),
-        
-            Forms\Components\Section::make('Información del Solicitante')
-                ->schema([
-                    Forms\Components\TextInput::make('nombre_apellido_solicitante')
-                        ->label('Nombre y Apellido del Solicitante')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('carnet_identidad')
-                        ->label('Carnet Identidad')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('celular')
-                        ->label('Celular')
-                        ->required()
-                        ->numeric(),
-        
-                    Forms\Components\TextInput::make('direccion')
-                        ->label('Dirección')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('numero')
-                        ->label('Número')
-                        ->required(),
-        
-                    Forms\Components\TextInput::make('zona')
-                        ->label('Zona')
-                        ->required(),
-                ]),
+        return $form->schema([
+            Wizard::make([
+                Wizard\Step::make('Información del Difunto')
+                    ->schema([
+                        TextInput::make('nombre_difunto')
+                            ->label('Nombre Difunto')
+                            ->required(),
+                
+                        Select::make('sexo')
+                            ->label('Sexo')
+                            ->options([
+                                'masculino' => 'Masculino',
+                                'femenino' => 'Femenino',
+                            ])
+                            ->required(),
+                
+                        TextInput::make('edad')
+                            ->label('Edad')
+                            ->required()
+                            ->numeric(),
+                
+                        Select::make('estado_civil')
+                            ->label('Estado Civil')
+                            ->options([
+                                'soltero' => 'Soltero(a)',
+                                'casado' => 'Casado(a)',
+                                'divorciado' => 'Divorciado(a)',
+                                'viudo' => 'Viudo(a)',
+                            ])
+                            ->required(),
+                
+                        TextInput::make('nacionalidad')
+                            ->label('Nacionalidad')
+                            ->required(),
+                
+                        TextInput::make('diagnostico_fallecimiento')
+                            ->label('Diagnóstico Fallecimiento')
+                            ->required(),
+                
+                        TextInput::make('medico')
+                            ->label('Médico')
+                            ->required(),
+                
+                        TextInput::make('orc')
+                            ->label('ORC')
+                            ->required(),
+                
+                        TextInput::make('libro')
+                            ->label('Libro')
+                            ->required(),
+                
+                        TextInput::make('folio')
+                            ->label('Folio')
+                            ->required(),
+                
+                        DatePicker::make('fecha_inhumacion')
+                            ->label('Fecha Inhumación')
+                            ->required(),
+                
+                        DatePicker::make('fecha_vencimiento')
+                            ->label('Fecha Vencimiento')
+                            ->required(),
+                
+                        TextInput::make('dia')
+                            ->label('Día')
+                            ->required(),
+                
+                        Textarea::make('descripcion_nicho')
+                            ->label('Descripción de Nicho')
+                            ->required(),
+                    ]),
+                
+                Wizard\Step::make('Ubicación')
+                    ->schema([
+                        TextInput::make('fila_ubicacion')
+                            ->label('Fila Ubicación')
+                            ->required()
+                            ->maxLength(255),
+                
+                        TextInput::make('sector_ubicacion')
+                            ->label('Sector Ubicación')
+                            ->required()
+                            ->maxLength(255),
+                
+                        TextInput::make('nro_ubicacion')
+                            ->label('Número Ubicación')
+                            ->required()
+                            ->maxLength(255),
+                    ]),
+                
+                Wizard\Step::make('Información del Solicitante')
+                    ->schema([
+                        TextInput::make('nombre_apellido_solicitante')
+                            ->label('Nombre y Apellido del Solicitante')
+                            ->required(),
+                
+                        TextInput::make('carnet_identidad')
+                            ->label('Carnet Identidad')
+                            ->required(),
+                
+                        TextInput::make('celular')
+                            ->label('Celular')
+                            ->required()
+                            ->numeric(),
+                
+                        TextInput::make('direccion')
+                            ->label('Dirección')
+                            ->required(),
+                
+                        TextInput::make('numero')
+                            ->label('Número')
+                            ->required(),
+                
+                        TextInput::make('zona')
+                            ->label('Zona')
+                            ->required(),
+                    ]),
+            ])
         ]);
-        
-    }
-
-    public static function table(Table $table): Table
-    {
-        return $table
-            ->columns([
-                Tables\Columns\TextColumn::make('nombre_difunto')
-                ->label('Nombre Difunto')
-                ->sortable()
-                ->searchable(),
-
-            Tables\Columns\TextColumn::make('nombre_apellido_solicitante')
-                ->label('Nombre y Apellido del Solicitante')
-                ->sortable()
-                ->searchable(),
-            ])
-            ->filters([
-                //
-            ])
-            ->actions([
-                Tables\Actions\EditAction::make(),
-            ])
-            ->bulkActions([
-                Tables\Actions\BulkActionGroup::make([
-                    Tables\Actions\DeleteBulkAction::make(),
-                ]),
-            ]);
     }
 
     public static function getRelations(): array
