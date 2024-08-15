62808cef207e0fca21ae58aaf5a670e242cc3a43
alanezfranz <alanezfranz@gmail.com>
2024-08-15 05:09:13 -0400

    cambio de password

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers\Filament;
 
+use App\Filament\Pages\Auth\EditProfile;
 use Filament\Http\Middleware\Authenticate;
 use Filament\Http\Middleware\DisableBladeIconComponents;
 use Filament\Http\Middleware\DispatchServingFilamentEvent;
@@ -29,6 +30,8 @@ class AdminPanelProvider extends PanelProvider
             ->id('admin')
             ->path('admin')
             ->login()
+            ->profile(EditProfile::class)
+            //->emailVerification()
             ->colors([
                 'primary' => Color::Blue,
                 'danger' => Color::Rose,
