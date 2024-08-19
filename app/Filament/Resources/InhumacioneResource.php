fbfa9987b1fcb5b4ca3ccd1825485caf340112f3
Franz Alanez <alanezflores@gmail.com>
2024-08-18 20:07:07 -0400

    cambios

--- app/Filament/Resources/InhumacioneResource.php
+++ app/Filament/Resources/InhumacioneResource.php
@@ -17,6 +17,7 @@ use Filament\Forms\Components\DatePicker;
 use Filament\Forms\Components\Textarea;
 use Filament\Forms\Components\FileUpload;
 use Filament\Tables\Actions\Action;
+use Filament\Forms\Components\Section;
 
 class InhumacioneResource extends Resource
 {
@@ -29,126 +30,140 @@ class InhumacioneResource extends Resource
     public static function form(Form $form): Form
     {
         return $form->schema([
-            Wizard::make([
-                Wizard\Step::make('Información del Difunto')
-                    ->schema([
-                        TextInput::make('nombre_difunto')
-                            ->label('Nombre Difunto')
-                            ->required(),
-
-                        Select::make('sexo')
-                            ->label('Sexo')
-                            ->options([
-                                'masculino' => 'Masculino',
-                                'femenino' => 'Femenino',
-                            ])
-                            ->required(),
-
-                        TextInput::make('edad')
-                            ->label('Edad')
-                            ->required()
-                            ->numeric(),
-
-                        Select::make('estado_civil')
-                            ->label('Estado Civil')
-                            ->options([
-                                'soltero' => 'Soltero(a)',
-                                'casado' => 'Casado(a)',
-                                'divorciado' => 'Divorciado(a)',
-                                'viudo' => 'Viudo(a)',
-                            ])
-                            ->required(),
-
-                        TextInput::make('nacionalidad')
-                            ->label('Nacionalidad')
-                            ->required(),
-
-                        TextInput::make('diagnostico_fallecimiento')
-                            ->label('Diagnóstico Fallecimiento')
-                            ->required(),
-
-                        TextInput::make('medico')
-                            ->label('Médico')
-                            ->required(),
-
+            Section::make('Información del Difunto')
+                ->schema([
+                    TextInput::make('nombre_difunto')
+                        ->label('Nombre Difunto')
+                        ->required(),
+                    
+                    Select::make('sexo')
+                        ->label('Sexo')
+                        ->options([
+                            'masculino' => 'Masculino',
+                            'femenino' => 'Femenino',
+                        ])
+                        ->required(),
+                    
+                    TextInput::make('edad')
+                        ->label('Edad')
+                        ->required()
+                        ->numeric(),
+                    
+                    Select::make('estado_civil')
+                        ->label('Estado Civil')
+                        ->options([
+                            'soltero' => 'Soltero(a)',
+                            'casado' => 'Casado(a)',
+                            'divorciado' => 'Divorciado(a)',
+                            'viudo' => 'Viudo(a)',
+                        ])
+                        ->required(),
+                    
+                    TextInput::make('nacionalidad')
+                        ->label('Nacionalidad')
+                        ->required(),
+                    
+                    TextInput::make('diagnostico_fallecimiento')
+                        ->label('Diagnóstico Fallecimiento')
+                        ->required(),
+                    
+                    TextInput::make('medico')
+                        ->label('Médico')
+                        ->required(),
+                    
                         TextInput::make('orc')
-                            ->label('ORC')
-                            ->required(),
-
-                        TextInput::make('libro')
-                            ->label('Libro')
-                            ->required(),
-
-                        TextInput::make('folio')
-                            ->label('Folio')
-                            ->required(),
-
-                        DatePicker::make('fecha_inhumacion')
-                            ->label('Fecha Inhumación')
-                            ->required(),
-
-                        DatePicker::make('fecha_vencimiento')
-                            ->label('Fecha Vencimiento')
-                            ->required(),
-
-                        TextInput::make('dia')
-                            ->label('Día')
-                            ->required(),
-
-                        Textarea::make('descripcion_nicho')
-                            ->label('Descripción de Nicho')
-                            ->required(),
-                    ]),
-
-                Wizard\Step::make('Ubicación')
-                    ->schema([
-                        TextInput::make('fila_ubicacion')
-                            ->label('Fila Ubicación')
-                            ->required()
-                            ->maxLength(255),
-
-                        TextInput::make('sector_ubicacion')
-                            ->label('Sector Ubicación')
-                            ->required()
-                            ->maxLength(255),
-
-                        TextInput::make('nro_ubicacion')
-                            ->label('Número Ubicación')
-                            ->required()
-                            ->maxLength(255),
-                    ]),
-
-                Wizard\Step::make('Información del Solicitante')
-                    ->schema([
-                        TextInput::make('nombre_apellido_solicitante')
-                            ->label('Nombre y Apellido del Solicitante')
-                            ->required(),
-
-                        TextInput::make('carnet_identidad')
-                            ->label('Carnet Identidad')
-                            ->required(),
-
-                        TextInput::make('celular')
-                            ->label('Celular')
-                            ->required()
-                            ->numeric(),
-
-                        TextInput::make('direccion')
-                            ->label('Dirección')
-                            ->required(),
-
-                        TextInput::make('numero')
-                            ->label('Número')
-                            ->required(),
-
-                        TextInput::make('zona')
-                            ->label('Zona')
-                            ->required(),
-
-                        FileUpload::make('comprobante_pdf')->label('Documento PDF')->disk('public')->directory('pdfs')->acceptedFileTypes(['application/pdf'])->required(),
-                    ]),
-            ])
+                        ->label('ORC')
+                        ->required(),
+                    
+                    TextInput::make('libro')
+                        ->label('Libro')
+                        ->required(),
+                    
+                    TextInput::make('folio')
+                        ->label('Folio')
+                        ->required(),    
+                ])
+                ->columns(2), // Configura 2 columnas para esta sección
+        
+            Section::make('Detalles del Registro')
+                ->schema([
+                 
+                    DatePicker::make('fecha_inhumacion')
+                        ->label('Fecha Inhumación')
+                        ->required(),
+                    
+                    DatePicker::make('fecha_vencimiento')
+                        ->label('Fecha Vencimiento')
+                        ->required(),
+                    
+                    TextInput::make('dia')
+                        ->label('Día')
+                        ->required(),
+                    
+                    Textarea::make('descripcion_nicho')
+                        ->label('Descripción de Nicho')
+                        ->required(),
+                ])
+                ->columns(2), // Configura 2 columnas para esta sección
+        
+            Section::make('Ubicación')
+                ->schema([
+                    TextInput::make('fila_ubicacion')
+                        ->label('Fila Ubicación')
+                        ->required()
+                        ->maxLength(255),
+                    
+                    TextInput::make('sector_ubicacion')
+                        ->label('Sector Ubicación')
+                        ->required()
+                        ->maxLength(255),
+                    
+                    TextInput::make('nro_ubicacion')
+                        ->label('Número Ubicación')
+                        ->required()
+                        ->maxLength(255),
+                ])
+                ->columns(2), // Configura 2 columnas para esta sección
+        
+            Section::make('Información del Solicitante')
+                ->schema([
+                    TextInput::make('nombre_apellido_solicitante')
+                        ->label('Nombre y Apellido del Solicitante')
+                        ->required(),
+                    
+                    TextInput::make('carnet_identidad')
+                        ->label('Carnet Identidad o Nit')
+                        ->required(),
+                    
+                    TextInput::make('celular')
+                        ->label('Celular')
+                        ->required()
+                        ->numeric()
+                        ->minLength(8)
+                        ->maxLength(8),
+                    
+                    TextInput::make('direccion')
+                        ->label('Dirección')
+                        ->required(),
+                    
+                    TextInput::make('numero')
+                        ->label('Número de domicilio')
+                        ->required(),
+                    
+                    TextInput::make('zona')
+                        ->label('Zona')
+                        ->required(),
+                    
+                    FileUpload::make('comprobante_pdf')
+                        ->label('Documento PDF')
+                        ->disk('public')
+                        ->directory('pdfs')
+                        ->acceptedFileTypes(['application/pdf'])
+                        ->required(),
+                ])
+                ->columns(2), // Configura 2 columnas para esta sección
         ]);
+        
     }
 
     public static function table(Table $table): Table
